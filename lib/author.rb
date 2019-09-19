69ff7d67cefa4ed9436cb0adbb047a6a7ebc40e1
christian williams <christian.williamsj@gmail.com>
2019-09-19 01:38:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,7 +17,7 @@ class Author
   
     def add_post_by_title(title)
       post = Post.new(title)
-        binding.pry
+        #binding.pry
         #self.posts << post
       post.author = self
     @@post_count +=1
